adc2dbc4b1e8b9b299eb858a141340f52356fa03
Rainik777 <Rainik_777@mail.ru>
2019-09-17 16:38:29 +0300

    Продолжение

--- js/start.js
+++ js/start.js
@@ -1,10 +1,22 @@
 'use strict';
 
+// Высота гистограммы 150px.
+// Ширина колонки 40px.
+// Расстояние между колонками 50px.
+// Цвет колонки игрока Вы rgba(255, 0, 0, 1).
+// Цвет колонок других игроков — синий, а насыщенность задаётся случайным образом.
+
 var STAT_CLOUD_WIDTH = 420;
 var STAT_CLOUD_HEIGHT = 270;
 var STAT_CLOUD_X = 100;
 var STAT_CLOUD_Y = 10;
 var GAP = 10;
+
+var statBlock = {
+  height: 150,
+  width: 40,
+  gap: 50
+};
 
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
@@ -19,4 +31,14 @@ window.renderStatistics = function (ctx) {
   ctx.textBaseline = 'hanging';
   ctx.fillText('Ура вы победили!', (STAT_CLOUD_X + STAT_CLOUD_WIDTH) / 2, STAT_CLOUD_Y + GAP);
   ctx.fillText('Список результатов:', (STAT_CLOUD_X + STAT_CLOUD_WIDTH) / 2 - 2 * GAP, STAT_CLOUD_Y + 3 * GAP);
+
+  ctx.fillStyle = '#000';
+  ctx.textBaseline = 'alphabetic';
+  ctx.fillText('You', 100 + statBlock.gap, 270);
+  ctx.fillRect(100 + statBlock.gap / 2, 250 - statBlock.height, statBlock.width, statBlock.height);
+
+  ctx.fillStyle = '#000';
+  ctx.textBaseline = 'alphabetic';
+  ctx.fillText('You2', 100 + statBlock.gap + statBlock.gap, 270);
+  ctx.fillRect(100 + statBlock.gap / 2, 250 - statBlock.height, statBlock.width, statBlock.height);
 };
